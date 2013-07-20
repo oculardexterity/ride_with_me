586a673468a47ce77a45cd8bdfdc7647567dc314
Richard Hadden <oculardexterity@gmail.com>
2013-07-20 15:23:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RideWithMe::Application.config.secret_key_base = '1a06b5b2c02677b76cfd726309eed7a5a136224efdfcb280d568b04b2401e59ad3f7c7af9a08eb99e432e06199755ae57b29e5f7117df24751d77a1ec70a32a8'
