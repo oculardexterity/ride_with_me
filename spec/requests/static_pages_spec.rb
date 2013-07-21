25e7512465917b94cb1e3f1fde33e8fc46ef819e
Richard Hadden <oculardexterity@gmail.com>
2013-07-21 11:58:46 +0100

    finished layout, added roots

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,53 +6,53 @@ describe "StaticPages" do
   
   describe "Home Page" do
   	it "should have the content 'Ride With Me'" do
-  		visit "/static_pages/home"
+  		visit root_path
   		expect(page).to have_content('Ride With Me')
   	end
 
   	it "should have a title containing Home" do
-  		visit "/static_pages/home"
+  		visit root_path
   		expect(page).to have_title("#{base_title}")
   	end
 
   	it "should not have a custom page title" do
-  		visit "/static_pages/home"
+  		visit root_path
   		expect(page).not_to have_title("#{base_title} | Home")
   	end
   end
 
   describe "Help Page" do
   	it "should have the content 'Help'" do
-  		visit "/static_pages/help"
+  		visit help_path
   		expect(page).to have_content("Help")
   	end
 
   	it "should have a title containing Help" do
-  		visit "/static_pages/help"
+  		visit help_path
   		expect(page).to have_title("#{base_title} | Help")
   	end
   end
 
   describe "About Page" do
   	it "should have the content 'About Ride With Me'" do
-  		visit "/static_pages/about"
+  		visit about_path
   		expect(page).to have_content("About Ride With Me")
   	end
 
   	it "should have a title containing About" do
-  		visit "/static_pages/about"
+  		visit about_path
   		expect(page).to have_title("#{base_title} | About")
   	end
   end
 
   describe "Contact Page" do
   	it "should have the content 'Contact Us'" do
-  		visit "/static_pages/contact"
+  		visit contact_path
   		expect(page).to have_content('Contact Us')
   	end
 
   	it "should have a title containing 'Contact'" do
-  		visit "/static_pages/contact"
+  		visit contact_path
   		expect(page).to have_title("#{base_title} | Contact")
   	end
   end
