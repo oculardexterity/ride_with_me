fc2d6f5453d60dc31ec1aaf4be72587f5e36693d
Richard Hadden <oculardexterity@gmail.com>
2013-08-03 15:11:57 +0100

    added post-signup tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  
   subject { page }
 
   describe "profile page" do
@@ -28,6 +27,13 @@ describe "UserPages" do
   		it "should not create a user" do
   			expect { click_button submit }.not_to change(User, :count)
   		end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
   	end
 
   	describe "with valid information" do
@@ -41,6 +47,15 @@ describe "UserPages" do
   		it "should create a user" do
   			expect { click_button submit }.to change(User, :count).by(1)
   		end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        let(:user) { User.find_by(email: "test@user.com") }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: "Hello #{user.name}") }
+      end
   	end
   end
 
