af125e5cea4d9bc526760fc37720e13ab7da71f5
Richard Hadden <oculardexterity@gmail.com>
2013-08-04 17:54:48 +0100

    done showing all users

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'jquery-rails', '2.2.1'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,31 @@ require 'spec_helper'
 describe "UserPages" do
   subject { page }
 
+  describe "index page" do
+    let(:test_user) { FactoryGirl.create(:user, name: "TestName", email: "test@user.com") }
+    before do
+      sign_in test_user
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+   describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name) unless user.name == test_user.name
+        end
+      end
+    end
+  end
+
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
